17c5bb33d5a5c965e9b4e0e36453b6c5a362e48b
Vo Quoc Dai <voquocdai.work@icloud.com>
2026-03-28 06:47:56 +0700

    Update solution for k-means-centroid-update

--- k-means-centroid-update/k-means-centroid-update.py
+++ k-means-centroid-update/k-means-centroid-update.py
@@ -11,16 +11,12 @@ def k_means_centroid_update(X, labels, k):
     new_centroids = []
 
     for j in range(k):
-        # Lọc các điểm thuộc cụm j bằng boolean masking
         cluster_points = X_arr[labels_arr == j]
         
         if len(cluster_points) > 0:
-            # Tính trung bình cộng theo từng chiều (axis=0)
             centroid = np.mean(cluster_points, axis=0)
-            # Chuyển từ NumPy array về list of floats
             new_centroids.append(centroid.tolist())
         else:
-            # Nếu cụm rỗng, trả về vector không (danh sách các số 0.0)
             zero_vector = [0.0] * n_features
             new_centroids.append(zero_vector)
             
